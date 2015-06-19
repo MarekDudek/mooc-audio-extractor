d2796060c45d3cda3f8937aec6179ff87f129a0a
Marek Dudek <marek.dudek@gmail.com>
2015-06-19 23:10:19 +0200

    Cleaned up

--- extract-audio-from-to.sh
+++ extract-audio-from-to.sh
@@ -3,4 +3,4 @@
 INPUT_REAL_PATH=$(realpath "$1")
 OUTPUT_REAL_PATH=$(realpath "$2")
 
-ffmpeg -i "${INPUT_REAL_PATH}" -map 0:a -c libmp3lame -q:a 2 -f mp3 "${OUTPUT_REAL_PATH}"
+ffmpeg -loglevel panic -i "${INPUT_REAL_PATH}" -map 0:a -c libmp3lame -q:a 2 -f mp3 "${OUTPUT_REAL_PATH}"
---
+++ process-elements.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+for ELEMENT in "$@"
+do
+	echo - processing \"$ELEMENT\"
+	OUTPUT_SUBDIR="${ELEMENT#$INPUT_DIR}"
+	if [ -d "$ELEMENT" ]; then
+		if [ "$ELEMENT" == "${INPUT_DIR}" ]; then
+			if [ -d "${OUTPUT_DIR}" ]; then
+				echo Error, output directory \"${INPUT_DIR}\" already exists!
+				exit 1
+			else
+				mkdir "${OUTPUT_DIR}"
+			fi
+		else
+			mkdir "${OUTPUT_DIR}/${OUTPUT_SUBDIR}"
+		fi
+	fi
+	if [ -f "$ELEMENT" ]; then
+		OUTPUT_FILE=${OUTPUT_DIR}/${OUTPUT_SUBDIR%.*}.mp3
+		./extract-audio-from-to.sh "${ELEMENT}" "${OUTPUT_FILE}"
+	fi
+done
