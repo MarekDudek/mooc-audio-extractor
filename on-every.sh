63856db99c552116dd97a501d795f32f69ed1d63
Marek Dudek <marek.dudek@gmail.com>
2015-06-19 22:22:23 +0200

    output directory hierarchy created

--- on-every.sh
+++ on-every.sh
@@ -1,14 +1,23 @@
 #!/bin/bash
 
-echo On every element
-echo output is ${OUTPUT_DIR}
-
-for path in "$@"
+for ELEMENT in "$@"
 do
-	if [ -d "$path" ]; then
-		echo - directory $path
+	if [ -d "$ELEMENT" ]; then
+		echo - directory $ELEMENT
+		if [ "$ELEMENT" == "${INPUT_DIR}" ]; then
+			echo -- root of the input hierarchy detected, creating root of output hierarchy
+			if [ -d "${OUTPUT_DIR}" ]; then
+				echo Error, output directory ${INPUT_DIR} already exists!
+				exit 1
+			else
+				mkdir "${OUTPUT_DIR}"
+			fi
+		else
+			OUTPUT_SUBDIR="${ELEMENT#$INPUT_DIR}"
+			mkdir "${OUTPUT_DIR}/${OUTPUT_SUBDIR}"
+		fi
 	fi
-	if [ -f "$path" ]; then
-		echo - file $path 
+	if [ -f "$ELEMENT" ]; then
+		echo - file $ELEMENT 
 	fi
 done
--- process-hierarhy.sh
+++ process-hierarhy.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-INPUT_DIR=$1
+export INPUT_DIR=$1
 export OUTPUT_DIR=$2
 
-echo Processing hierarchy
-echo From $INPUT_DIR to $OUTPUT_DIR
+echo Processing hierarchy from $INPUT_DIR to $OUTPUT_DIR
 
 find "${INPUT_DIR}" -print0 | xargs -0 ./on-every.sh
